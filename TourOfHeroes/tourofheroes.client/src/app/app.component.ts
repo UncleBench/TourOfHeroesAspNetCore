f4afc04fb92f89cd4c37174332bf9d5ae29f9fe8
UncleBench <sali_vali@hotmail.com>
2024-01-30 15:31:47 +0100

    Refactor AppComponent

--- TourOfHeroes/tourofheroes.client/src/app/app.component.ts
+++ TourOfHeroes/tourofheroes.client/src/app/app.component.ts
@@ -1,42 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 const appTitle: string = 'Tour of Heroes';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-  public title: string;
+export class AppComponent {
+  title: string;
 
-  constructor(private http: HttpClient, private titleService: Title) {
+  constructor() {
+    let titleService = Inject(Title);
     titleService.setTitle(appTitle);
     this.title = titleService.getTitle();
-  }
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
   }
 }
