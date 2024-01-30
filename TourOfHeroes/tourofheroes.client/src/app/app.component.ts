e4a0bb528f0e05f727b5eb06d5430e693ad307d6
UncleBench <sali_vali@hotmail.com>
2024-01-30 15:04:50 +0100

    Properly set the app's title using the Title service

--- TourOfHeroes/tourofheroes.client/src/app/app.component.ts
+++ TourOfHeroes/tourofheroes.client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+const appTitle: string = 'Tour of Heroes';
 
 interface WeatherForecast {
   date: string;
@@ -15,8 +18,12 @@ interface WeatherForecast {
 })
 export class AppComponent implements OnInit {
   public forecasts: WeatherForecast[] = [];
+  public title: string;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private titleService: Title) {
+    titleService.setTitle(appTitle);
+    this.title = titleService.getTitle();
+  }
 
   ngOnInit() {
     this.getForecasts();
@@ -32,6 +39,4 @@ export class AppComponent implements OnInit {
       }
     );
   }
-
-  title = 'tourofheroes.client';
 }
